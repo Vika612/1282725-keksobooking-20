12bb4564d33e508501f6b56ae5d7cc3c579d6e01
Victoria Domovets <v.domovets@gmail.com>
2020-07-15 15:19:08 +0200

    filter price

--- js/filter.js
+++ js/filter.js
@@ -2,10 +2,17 @@
 
 (function () {
   var DEFAULT_FILTER = 'any';
+  var Price = {
+    LOW: 'low',
+    MIDDLE: 'middle',
+    HIGH: 'high',
+    MIN: 10000,
+    MAX: 50000
+  };
 
   var mapFilters = document.querySelector('.map__filters');
   var housingType = mapFilters.querySelector('#housing-type');
-  // var housingPrice = mapFilters.querySelector('#housing-price');
+  var housingPrice = mapFilters.querySelector('#housing-price');
   var housingRooms = mapFilters.querySelector('#housing-rooms');
   var housingGuests = mapFilters.querySelector('#housing-guests');
 
@@ -22,8 +29,29 @@
     return housingGuests.value === DEFAULT_FILTER || Number(housingGuests.value) === adv.offer.guests;
   };
 
+  var filterHousingPrice = function (adv) {
+    switch (housingPrice.value) {
+      case Price.LOW:
+        return adv.offer.price < Price.MIN;
+      case Price.MIDDLE:
+        return adv.offer.price >= Price.MIN && adv.offer.price < Price.MAX;
+      case Price.HIGH:
+        return adv.offer.price >= Price.MAX;
+      default:
+        return DEFAULT_FILTER;
+    }
+  };
+
+  var filters = [filterHousingType, filterHousingPrice, filterHousingRooms, filterHousingGuests];
+
+  var checkFilter = function (adv) {
+    return filters.every(function (filter) {
+      return filter(adv);
+    });
+  };
+
   var updateOffers = function () {
-    var filterValue = window.mainPin.getOffers().filter(filterHousingType).filter(filterHousingRooms).filter(filterHousingGuests);
+    var filterValue = window.mainPin.getOffers().filter(checkFilter);
     window.map.generatePins(filterValue);
   };
 
