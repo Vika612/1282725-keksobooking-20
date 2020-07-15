1b24c4c86d2f8bd7e17800f657859aa8eb60dc71
Victoria Domovets <v.domovets@gmail.com>
2020-07-15 14:23:07 +0200

    filter rooms/guests

--- js/filter.js
+++ js/filter.js
@@ -5,13 +5,25 @@
 
   var mapFilters = document.querySelector('.map__filters');
   var housingType = mapFilters.querySelector('#housing-type');
+  // var housingPrice = mapFilters.querySelector('#housing-price');
+  var housingRooms = mapFilters.querySelector('#housing-rooms');
+  var housingGuests = mapFilters.querySelector('#housing-guests');
+
 
   var filterHousingType = (function (adv) {
     return housingType.value === DEFAULT_FILTER || housingType.value === adv.offer.type;
   });
 
+  var filterHousingRooms = function (adv) {
+    return housingRooms.value === DEFAULT_FILTER || Number(housingRooms.value) === adv.offer.rooms;
+  };
+
+  var filterHousingGuests = function (adv) {
+    return housingGuests.value === DEFAULT_FILTER || Number(housingGuests.value) === adv.offer.guests;
+  };
+
   var updateOffers = function () {
-    var filterValue = window.mainPin.getOffers().filter(filterHousingType);
+    var filterValue = window.mainPin.getOffers().filter(filterHousingType).filter(filterHousingRooms).filter(filterHousingGuests);
     window.map.generatePins(filterValue);
   };
 
